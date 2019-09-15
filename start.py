cf3c15d44d0f3b0c36e54959af771981a18f8072
eljsus <55217475+eljsus@users.noreply.github.com>
2019-09-14 23:02:19 -0500

    Create start.py

---
+++ start.py
@@ -0,0 +1,96 @@
+import os
+import time
+os.system("apt install pip3")
+try:
+	import requests
+	print("""
+
+			[!] libreria requests instalada!
+
+			""")
+except:
+	print("""
+
+		libreria requests no instalada, instalando...
+
+			""")
+	os.system("pip3 install requests")
+
+
+
+
+def cmd(command):
+    os.system(command)
+
+    
+    
+
+print("Iniciando Setup de Linux")
+print("""
+		Ingresa "s,y,si," para continuar.
+		Ingresa "n,no" para salir.
+		""")
+pregunta1= input("Desea Continuar?: ")
+
+if pregunta1 == "s" or "y" or "si":
+    pass
+else:
+    exit()
+
+for x in range(0,101,5):
+    print("\n")
+    print("         \\|||||/        ")
+    print("         ( O O )         ")
+    print("|--ooO-----(_)----------|")
+    print("|                       |")
+    print(f"|    Comenzando {x}%     |")
+    print("|                       |")
+    print("|------------------Ooo--|")
+    print("         |__||__|        ")
+    print("          ||  ||         ")
+    print("         ooO  Ooo        ")
+    time.sleep(1)
+    cmd("clear")
+
+
+time.sleep(1)
+cmd("clear")
+print("Actualizando...")
+cmd("clear")
+cmd("apt update && y")
+time.sleep(1)
+print("Actualizando...")
+cmd("apt upgrade && y")
+time.sleep(1)
+try:
+        cmd("apt-cache policy nano")
+        print("""
+
+			[!] editor nano instalado!
+
+			""")
+except:
+        cmd("apt install nano && y")
+        print("""
+
+			[x] no tiene nano instalado, instalando...
+
+			""")
+time.sleep(1)
+print("Configurando sshd_config para aceptar contraseñas...")
+#cmd("nano /etc/ssh/sshd_config")
+time.sleep(3)
+print("Cambiando Contraseña:")
+cmd("clear")
+print("Ingrese su contraseña 2 veces:")
+cmd("passwd")
+print("Contraseña cambiada con exito!")
+time.sleep(3)
+cmd("clear")
+print("Restaurando sshd y ssh ...")
+cmd("service sshd restart")
+cmd("service ssh restart")
+time.sleep(3)
+print("""
+        Listo!
+        """)
